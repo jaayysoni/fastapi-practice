92df858d57bb9d9a1ff7aa139709181cd84fd7dc
JayySoni <sonijaysoni459@gmail.com>
2026-05-09 14:44:59 +0530

    add two sum API with query parameter input

--- GET/query_parameters.py
+++ GET/query_parameters.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 
 app = FastAPI()
 
@@ -6,9 +6,16 @@ app = FastAPI()
 def count_char(text: str, char: str):
     if len(char) != 1:
         raise HTTPException(status_code=400, detail="Parameter 'char' must be a single character.")
-    else:
-        return {"character": char, "count": text.count(char)}
+    return {"character": char, "count": text.count(char)}
 
 
-
-
+@app.get("/twosum")
+def two_sum(nums: str, target: int):
+    nums = [int(x) for x in nums.split(",")]
+    seen = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            return {"index1": seen[diff], "index2": i}
+        seen[num] = i
+    raise HTTPException(status_code=404, detail="No two sum solution found.")
