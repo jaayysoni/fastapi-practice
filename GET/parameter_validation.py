8dd7ceb417b2075188534e083abbd50fb91f24c3
JayySoni <sonijaysoni459@gmail.com>
2026-05-11 16:18:40 +0530

    Numeric Constraints added in GET/parameter_validation.py

---
+++ GET/parameter_validation.py
@@ -0,0 +1,29 @@
+# GET/parameter_validation.py 
+from fastapi import FastAPI, Query, Path
+
+app = FastAPI()
+
+
+@app.get("/items/")
+def get_items(limit: int = Query(default=10, ge=1, le=100)):
+    return {"limit": limit}
+    
+@app.get("/users/{user_id}")
+def get_user(user_id: int = Path(..., gt=1)):
+    return {"user_id": user_id}
+
+@app.get("/products/")
+def get_products(price: int = Query(default = 10,lt = 50)):
+    return {"price": price}
+
+
+
+
+
+
+
+
+
+
+
+
