4d949db182ba377ec4fae433a31a0779be052bcf
JayySoni <sonijaysoni459@gmail.com>
2026-05-17 19:58:30 +0530

    List Params API added

--- GET/parameter_validation.py
+++ GET/parameter_validation.py
@@ -1,5 +1,6 @@
 # GET/parameter_validation.py 
 from fastapi import FastAPI, Query, Path
+from typing import List
 
 app = FastAPI()
 
@@ -16,13 +17,11 @@ def get_user(user_id: int = Path(..., gt=1)):
 def get_products(price: int = Query(default = 10,lt = 50)):
     return {"price": price}
 
-
 @app.get("/orders/")
 def get_orders(
     status: str = Query(...),
     page: int = Query(default=1, ge=1)):
     return {"status": status,"page": page}
-
 
 @app.get("/reports/")
 def get_reports(
@@ -36,13 +35,11 @@ def get_reports(
 ):
     return {"year": year}
 
-
 @app.get("/products/search/")
 def search_products(
     name: str = Query(...,alias= "item-name")
 ):
     return {"name": name}
-
 
 @app.get("/legacy/items/")
 def legacy_items(
@@ -52,7 +49,37 @@ def legacy_items(
     return {"limit": limit, "page_size": page_size}
 
 
+@app.get("/items/")
+def get_items(tag: List[str] = Query(default = [])):
+    return {"tag": tag}
 
+@app.get("/search/")
+def search(ids: List[int] = Query(...)):
+    return {"ids": ids}
+
+@app.get("/batch/")
+def batch(ids: List[int] = Query(default = [], min_lenght = 1, max_lenght = 10)):
+    return {"ids": ids, "count": len(ids)}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#7 List Params    ⏳
+#8 Enum Params    ⏳
 
 
 
