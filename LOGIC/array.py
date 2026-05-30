d92f1b8ca0adb1e33787c8ebf7b3b67788197d3f
JayySoni <sonijaysoni459@gmail.com>
2026-05-30 14:48:58 +0530

    another added in array

--- LOGIC/array.py
+++ LOGIC/array.py
@@ -11,7 +11,27 @@ for i in a.split():
 print(c)
 
 
+# given two strings, find word that are in a but not in b
 
+a = "my name is jay"
+b = "my name is akshey"
+c = []
+
+for i in a.split():
+    if i not in b.split():
+        c.append(i)
+print(c)
+
+
+# given a sentence remove all duplicate words and print the result
+
+sentence = "jay is a good developer and jay loves to code and is happy"
+
+result = []
+for i in sentence.split():
+    if i not in result:
+        result.append(i)
+print(result)
 
 
 
